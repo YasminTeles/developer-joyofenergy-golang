5670fce51ed9f98b2d6b86bac736cde42278deef
Yasmin Teles <teles.yasmin.tech@gmail.com>
2024-05-01 22:35:11 -0300

    refactor: readings validations

--- endpoints/readings/validation_test.go
+++ endpoints/readings/validation_test.go
@@ -2,50 +2,23 @@ package readings
 
 import (
 	"testing"
-	"time"
 
 	"github.com/stretchr/testify/assert"
-
-	"joi-energy-golang/domain"
 )
 
-func generateValidInput() domain.StoreReadings {
-	return domain.StoreReadings{
-		SmartMeterId: "12345",
-		ElectricityReadings: []domain.ElectricityReading{{
-			Time:    time.Now(),
-			Reading: 123.45,
-		}},
-	}
-}
-
-func TestSuccessfulValidation(t *testing.T) {
+func TestValidateSmartMeterIdEmpty(t *testing.T) {
 	t.Parallel()
 
-	input := generateValidInput()
+	err := validateSmartMeterId("")
 
-	err := validateStoreReadings(input)
+	assert.Error(t, err)
+	assert.EqualError(t, err, "cannot be blank")
+}
+
+func TestValidateSmartMeterId(t *testing.T) {
+	t.Parallel()
+
+	err := validateSmartMeterId("smartMeterId")
+
 	assert.NoError(t, err)
-}
-
-func TestValidationFailureWithMissingID(t *testing.T) {
-	t.Parallel()
-
-	input := generateValidInput()
-	input.SmartMeterId = ""
-
-	err := validateStoreReadings(input)
-	expectedErr := "store readings validation failed: smartMeterId: cannot be blank."
-	assert.EqualError(t, err, expectedErr)
-}
-
-func TestValidationFailureWithMissingData(t *testing.T) {
-	t.Parallel()
-
-	input := generateValidInput()
-	input.ElectricityReadings = nil
-
-	err := validateStoreReadings(input)
-	expectedErr := "store readings validation failed: electricityReadings: is required."
-	assert.EqualError(t, err, expectedErr)
 }
